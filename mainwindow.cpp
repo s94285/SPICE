edcc4f963851a00f83e1c3095a9ec01977f3fbb7
s94285 <s94285@yahoo.com.tw>
2018-11-25 15:28:27 +0800

    Add mainToolbar Buttons

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,25 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    //make toolbar buttons
+    short int buttonIndex[8]={4,15,21,22,24,25,26,29};
+    for(int i=0;i<8;i++){
+        QPushButton *qb=new QPushButton;
+        toolBarButton[i]=qb;
+        qb->setFixedSize(30,30);
+        qb->setIconSize(QSize(24,22));
+        QPixmap qp(":/resources/button.png");
+        QPixmap croped=qp.copy(QRect(buttonIndex[i]*24,0,24,22));
+        qb->setIcon(croped);
+        ui->mainToolBar->addWidget(qb);
+    }
+    //
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+    for(auto tb:toolBarButton){
+        delete tb;
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,7 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <QPushButton>
 namespace Ui {
 class MainWindow;
 }
@@ -17,6 +17,7 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QPushButton *toolBarButton[8];
 };
 
 #endif // MAINWINDOW_H
