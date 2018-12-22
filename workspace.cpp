a968941b3d8c6011ecc5f7dfbe4d7370dd81f28c
s94285 <s94285@yahoo.com.tw>
2018-12-22 17:47:32 +0800

    Fix the bug that clicks only on node but items below.

--- workspace.cpp
+++ workspace.cpp
@@ -61,169 +61,190 @@ void Workspace::enterEvent(QEvent *event){
 
 }
 void Workspace::mousePressEvent(QMouseEvent *event){
-    if(itemAt(event->pos()))qDebug() << "Clicked On Item";
-    if(dynamic_cast<Node*>(itemAt(event->pos())))qDebug() << "Clicked on node";
-    if(dynamic_cast<Line*>(itemAt(event->pos())))qDebug() << "Clicked on line with node: " << dynamic_cast<Node*>(dynamic_cast<Line*>(itemAt(event->pos()))->group());
-    if(event->button()==Qt::RightButton&&(itemAt(event->pos())==nullptr)){
-        currentMode=IDLE;
-        viewport()->setCursor(Qt::CrossCursor);
-
-        return;
-    }
-    if(event->button()==Qt::RightButton&&drawingline){
-        //cancel drawingline
-        Node *nodeOfLineToDelete=dynamic_cast<Node*>(drawingline->group());
-        for(QGraphicsItem *line:nodeOfLineToDelete->childItems()){
-            lines->removeOne(dynamic_cast<Line*>(line));
-            nodeOfLineToDelete->removeFromGroup(line);
-            delete line;
+    QGraphicsItem *itemOnTop=nullptr;
+    for(QGraphicsItem *i:items(event->pos()))
+        if(dynamic_cast<Node*>(i)==nullptr){ //click through all node
+            itemOnTop=i;
+            break;
         }
-        nodes->removeOne(nodeOfLineToDelete);
-        delete nodeOfLineToDelete;
-        drawingline=nullptr;
-        scene->update();
-    }
-    if(event->button()==Qt::RightButton&&itemAt(event->pos())!=nullptr)
-    {
-        if(currentMode==DRAWLINE){
+    if(itemOnTop)qDebug() << "Clicked On Item";
+        if(dynamic_cast<Node*>(itemOnTop))qDebug() << "Clicked on node";
+        if(dynamic_cast<Line*>(itemOnTop))qDebug() << "Clicked on line with node: " << dynamic_cast<Node*>(dynamic_cast<Line*>(itemOnTop)->group());
+        if(event->button()==Qt::RightButton&&(itemOnTop==nullptr)){
             currentMode=IDLE;
             viewport()->setCursor(Qt::CrossCursor);
-            if(drawLineX){scene->removeItem(drawLineX);delete drawLineX;drawLineX=nullptr;}
-            if(drawLineY){scene->removeItem(drawLineY);delete drawLineY;drawLineY=nullptr;}
-            return;
-        }
-       /* Resistor *rptr;
-        Inductor *iptr;
-        Capacitor *cptr; */
-        /*if(dynamic_cast<Resistor*>(itemAt(event->pos())))
-        {
-            dialog=new editDialog(this,dynamic_cast<Resistor*>(itemAt(event->pos())));
-        }
-        else if(dynamic_cast<Inductor*>(itemAt(event->pos())))
-        {
-            dialog=new editDialog(this,dynamic_cast<Inductor*>(itemAt(event->pos())));
-        }
-        else
-        {
-            dialog=new editDialog(this,dynamic_cast<Capacitor*>(itemAt(event->pos())));
-        }*/
-        if(dynamic_cast<Resistor*>(itemAt(event->pos()))!=nullptr||dynamic_cast<Inductor*>(itemAt(event->pos()))!=nullptr||dynamic_cast<Capacitor*>(itemAt(event->pos()))!=nullptr)
-        {
-            dialog=new editDialog(this,dynamic_cast<BasicComponent*>(itemAt(event->pos())));
-            currentMode=IDLE;
-            dialog->exec();
-        }
-        else if(dynamic_cast<Source*>(itemAt(event->pos()))!=nullptr)
-        {
-            sourcedialog=new SourceDialog(this,dynamic_cast<Source*>(itemAt(event->pos())));
-            currentMode=IDLE;
-            sourcedialog->exec();
-        }
-        else
-            currentMode=IDLE;
 
-    }
-    switch(currentMode){
-        case IDLE:
-        if(event->button()==Qt::LeftButton){
-            _pan=true;
-            _panStartX=event->x();
-            _panStartY=event->y();
-            viewport()->setCursor(Qt::ClosedHandCursor);
-            event->accept();
             return;
         }
-        break;
-        case MOVE:
-        if(itemSelected){   //selected
-            itemSelected=nullptr;
-        }else{      //unselected
-            itemSelected=dynamic_cast<BasicComponent*>(itemAt(event->pos()));
-            qDebug()<<"selected12345\n";
+        if(event->button()==Qt::RightButton&&drawingline){
+            //cancel drawingline
+            Node *nodeOfLineToDelete=dynamic_cast<Node*>(drawingline->group());
+            for(QGraphicsItem *line:nodeOfLineToDelete->childItems()){
+                lines->removeOne(dynamic_cast<Line*>(line));
+                nodeOfLineToDelete->removeFromGroup(line);
+                delete line;
+            }
+            nodes->removeOne(nodeOfLineToDelete);
+            delete nodeOfLineToDelete;
+            drawingline=nullptr;
+            scene->update();
         }
-        break;
-        case CUT:
+        if(event->button()==Qt::RightButton&&itemOnTop!=nullptr)
         {
-        BasicComponent *toDelete=dynamic_cast<BasicComponent*>(itemAt(event->pos()));
-        Line *lineToDelete=dynamic_cast<Line*>(itemAt(event->pos()));
-        if(toDelete){components->removeOne(toDelete);delete toDelete;}
-        if(lineToDelete){
-            Node *nodeOfLineToDelete=dynamic_cast<Node*>(lineToDelete->group());
-            for(int i=0;i<nodeOfLineToDelete->connectedPorts.size();i++){
-                //if current lineToDelete connects to a port, then this port should be deleted from node
-                if((*(nodeOfLineToDelete->connectedPorts.at(i).second))==lineToDelete->getPoint1()
-                        ||(*(nodeOfLineToDelete->connectedPorts.at(i).second))==lineToDelete->getPoint2()){
-                    nodeOfLineToDelete->connectedPorts.erase(nodeOfLineToDelete->connectedPorts.begin()+i);
-                    i--;
-                }
+            if(currentMode==DRAWLINE){
+                currentMode=IDLE;
+                viewport()->setCursor(Qt::CrossCursor);
+                if(drawLineX){scene->removeItem(drawLineX);delete drawLineX;drawLineX=nullptr;}
+                if(drawLineY){scene->removeItem(drawLineY);delete drawLineY;drawLineY=nullptr;}
+                return;
             }
-            //delete line
-            qDebug() << "delete line";
-            lines->removeOne(lineToDelete);
-            nodeOfLineToDelete->removeFromGroup(lineToDelete);
-            delete lineToDelete;
-            //delete the node if none of port exist
-            if(nodeOfLineToDelete->connectedPorts.empty()){
-                qDebug() << "delete the node";
-                for(QGraphicsItem *line:nodeOfLineToDelete->childItems()){
-                    qDebug() << "delete lines in node";
-                    lines->removeOne(dynamic_cast<Line*>(line));
-                    delete line;
-                }
-                nodes->removeOne(nodeOfLineToDelete);
-                delete nodeOfLineToDelete;
+           /* Resistor *rptr;
+            Inductor *iptr;
+            Capacitor *cptr; */
+            /*if(dynamic_cast<Resistor*>(itemOnTop))
+            {
+                dialog=new editDialog(this,dynamic_cast<Resistor*>(itemOnTop));
             }
-            qDebug() << "delete finish";
+            else if(dynamic_cast<Inductor*>(itemOnTop))
+            {
+                dialog=new editDialog(this,dynamic_cast<Inductor*>(itemOnTop));
+            }
+            else
+            {
+                dialog=new editDialog(this,dynamic_cast<Capacitor*>(itemOnTop));
+            }*/
+            if(dynamic_cast<Resistor*>(itemOnTop)!=nullptr||dynamic_cast<Inductor*>(itemOnTop)!=nullptr||dynamic_cast<Capacitor*>(itemOnTop)!=nullptr)
+            {
+                dialog=new editDialog(this,dynamic_cast<BasicComponent*>(itemOnTop));
+                currentMode=IDLE;
+                dialog->exec();
+            }
+            else if(dynamic_cast<Source*>(itemOnTop)!=nullptr)
+            {
+                sourcedialog=new SourceDialog(this,dynamic_cast<Source*>(itemOnTop));
+                currentMode=IDLE;
+                sourcedialog->exec();
+            }
+            else
+                currentMode=IDLE;
+
         }
-        break;
-        }
-        case DRAWLINE:
-        {
-        QGraphicsItem *itemClicked=nullptr;
-        for(QGraphicsItem *i:items(event->pos()))
-            if(i!=drawLineX&&i!=drawLineY&&i!=drawingline&&dynamic_cast<Node*>(i)==nullptr){ //when drawingline, click through current line and nodes
-                itemClicked=i;
-                break;
+        switch(currentMode){
+            case IDLE:
+            if(event->button()==Qt::LeftButton){
+                _pan=true;
+                _panStartX=event->x();
+                _panStartY=event->y();
+                viewport()->setCursor(Qt::ClosedHandCursor);
+                event->accept();
+                return;
             }
-        qDebug() << "DrawLineClicked\n";
-        if(itemClicked!=nullptr){
-            qDebug() << "itemClicked\n";
-            BasicComponent *componentClicked=dynamic_cast<BasicComponent*>(itemClicked);
-            Line *lineClicked=dynamic_cast<Line*>(itemClicked);
-            if(componentClicked){
-                qDebug() << "componentClicked\n" << mapToScene(event->pos()) << endl;
-                QPoint *portClicked=componentClicked->getPortByScenePoint(mapToScene(event->pos()));
-                if(portClicked){    //null if not found
-                    qDebug() << "portClicked\n";
-                    if(drawingline){
-                        //finish the line
-                        drawingline->setPoint2(mapToScene(event->pos()));
-                        Node *node=dynamic_cast<Node*>(drawingline->group());
-                        node->connectedPorts.append(Node::PortOfComponent(componentClicked,portClicked));
-                        drawingline=nullptr;
-                        scene->update();
-                    }else{
-                        //create a new node and draw a line
-                        Node *newNode=new Node();
-                        QPointF pointFClicked=mapToScene(event->pos());
-                        drawingline=new Line(pointFClicked,pointFClicked);
-                        newNode->addToGroup(drawingline);
-                        drawingline->setGroup(newNode);
-//                        drawingline->group()
-                        nodes->append(newNode);
-                        lines->append(drawingline);
-                        newNode->connectedPorts.append(Node::PortOfComponent(componentClicked,portClicked));
-                        this->drawComponents();
+            break;
+            case MOVE:
+            if(itemSelected){   //selected
+                itemSelected=nullptr;
+            }else{      //unselected
+                itemSelected=dynamic_cast<BasicComponent*>(itemOnTop);
+                qDebug()<<"selected12345\n";
+            }
+            break;
+            case CUT:
+            {
+            BasicComponent *toDelete=dynamic_cast<BasicComponent*>(itemOnTop);
+            Line *lineToDelete=dynamic_cast<Line*>(itemOnTop);
+            if(toDelete){components->removeOne(toDelete);delete toDelete;}
+            if(lineToDelete){
+                Node *nodeOfLineToDelete=dynamic_cast<Node*>(lineToDelete->group());
+                for(int i=0;i<nodeOfLineToDelete->connectedPorts.size();i++){
+                    //if current lineToDelete connects to a port, then this port should be deleted from node
+                    if((*(nodeOfLineToDelete->connectedPorts.at(i).second))==lineToDelete->getPoint1()
+                            ||(*(nodeOfLineToDelete->connectedPorts.at(i).second))==lineToDelete->getPoint2()){
+                        nodeOfLineToDelete->connectedPorts.erase(nodeOfLineToDelete->connectedPorts.begin()+i);
+                        i--;
                     }
                 }
-            }else if(lineClicked){  //need to draw blue rectangle for articulation
-                qDebug() << "Clicked on another Line\n";
-                if(drawingline==nullptr){    //only accept line for simplicy
-                    //add a new line with same node
-                    Node *node=dynamic_cast<Node*>(lineClicked->group());
+                //delete line
+                qDebug() << "delete line";
+                lines->removeOne(lineToDelete);
+                nodeOfLineToDelete->removeFromGroup(lineToDelete);
+                delete lineToDelete;
+                //delete the node if none of port exist
+//                if(nodeOfLineToDelete->connectedPorts.empty()){
+                    qDebug() << "delete the node";
+                    for(QGraphicsItem *line:nodeOfLineToDelete->childItems()){
+                        qDebug() << "delete lines in node";
+                        lines->removeOne(dynamic_cast<Line*>(line));
+                        delete line;
+                    }
+                    nodes->removeOne(nodeOfLineToDelete);
+                    delete nodeOfLineToDelete;
+//                }
+                qDebug() << "delete finish";
+                scene->update();
+            }
+            break;
+            }
+            case DRAWLINE:
+            {
+            QGraphicsItem *itemClicked=nullptr;
+            for(QGraphicsItem *i:items(event->pos()))
+                if(i!=drawLineX&&i!=drawLineY&&i!=drawingline&&dynamic_cast<Node*>(i)==nullptr){ //when drawingline, click through current line and nodes
+                    itemClicked=i;
+                    break;
+                }
+            qDebug() << "DrawLineClicked\n";
+            if(itemClicked!=nullptr){
+                qDebug() << "itemClicked\n";
+                BasicComponent *componentClicked=dynamic_cast<BasicComponent*>(itemClicked);
+                Line *lineClicked=dynamic_cast<Line*>(itemClicked);
+                if(componentClicked){
+                    qDebug() << "componentClicked\n" << mapToScene(event->pos()) << endl;
+                    QPoint *portClicked=componentClicked->getPortByScenePoint(mapToScene(event->pos()));
+                    if(portClicked){    //null if not found
+                        qDebug() << "portClicked\n";
+                        if(drawingline){
+                            //finish the line
+                            drawingline->setPoint2(mapToScene(event->pos()));
+                            Node *node=dynamic_cast<Node*>(drawingline->group());
+                            node->connectedPorts.append(Node::PortOfComponent(componentClicked,portClicked));
+                            drawingline=nullptr;
+                            scene->update();
+                        }else{
+                            //create a new node and draw a line
+                            Node *newNode=new Node();
+                            QPointF pointFClicked=mapToScene(event->pos());
+                            drawingline=new Line(pointFClicked,pointFClicked);
+                            newNode->addToGroup(drawingline);
+                            drawingline->setGroup(newNode);
+    //                        drawingline->group()
+                            nodes->append(newNode);
+                            lines->append(drawingline);
+                            newNode->connectedPorts.append(Node::PortOfComponent(componentClicked,portClicked));
+                            this->drawComponents();
+                        }
+                    }
+                }else if(lineClicked){  //need to draw blue rectangle for articulation
+                    qDebug() << "Clicked on another Line\n";
+                    if(drawingline==nullptr){    //only accept line for simplicy
+                        //add a new line with same node
+                        Node *node=dynamic_cast<Node*>(lineClicked->group());
+                        if(node){
+                            drawingline=new Line(mapToScene(event->pos()),mapToScene(event->pos()));
+                            drawingline->setPoint1Rect(true);
+                            lines->append(drawingline);
+                            node->addToGroup(drawingline);
+                            drawingline->setGroup(node);
+                            scene->update();
+                        }else
+                            qDebug() << "didn't get node";
+                    }
+                }
+            }else{  //itemcliked == nullptr
+                qDebug() << "Clicked outside";
+                if(drawingline){//finish this line and create new line
+                    Node *node=dynamic_cast<Node*>(drawingline->group());
                     if(node){
-                        drawingline=new Line(mapToScene(event->pos()),mapToScene(event->pos()));
-                        drawingline->setPoint1Rect(true);
+                        drawingline=new Line(drawingline->getPoint2(),drawingline->getPoint2());
                         lines->append(drawingline);
                         node->addToGroup(drawingline);
                         drawingline->setGroup(node);
@@ -232,25 +253,11 @@ void Workspace::mousePressEvent(QMouseEvent *event){
                         qDebug() << "didn't get node";
                 }
             }
-        }else{  //itemcliked == nullptr
-            qDebug() << "Clicked outside";
-            if(drawingline){//finish this line and create new line
-                Node *node=dynamic_cast<Node*>(drawingline->group());
-                if(node){
-                    drawingline=new Line(drawingline->getPoint2(),drawingline->getPoint2());
-                    lines->append(drawingline);
-                    node->addToGroup(drawingline);
-                    drawingline->setGroup(node);
-                    scene->update();
-                }else
-                    qDebug() << "didn't get node";
+                break;
             }
         }
-            break;
-        }
-    }
-    QGraphicsView::mousePressEvent(event);
-//    viewport()->setCursor(Qt::CrossCursor);   //override default pan cursor
+        QGraphicsView::mousePressEvent(event);
+    //    viewport()->setCursor(Qt::CrossCursor);   //override default pan cursor
 }
 void Workspace::mouseMoveEvent(QMouseEvent *event){
     //for drawLine X Y axis
